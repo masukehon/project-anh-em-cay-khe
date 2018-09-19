4e47c37d4706f36df32053147a729ea903e29ccc
Kha Cao Vinh <masuke96@gmail.com>
2018-09-19 13:59:24 +0700

    employee sign up, sign in

--- src/controllers/admin/admin.route.js
+++ src/controllers/admin/admin.route.js
@@ -1,9 +1,23 @@
 const { Router } = require("express");
+const { EmployeeService } = require("../../services/employee.service");
 
 const adminRouter = Router();
 
 adminRouter.get('/', (req, res, next) => {
-    res.render('admin/masterPage');
+    res.send('admin.ok');
 });
 
+adminRouter.post('/signup',(req, res, next) => {
+    const {name,email,password} = req.body;
+    EmployeeService.SignUp(name, email, password)
+    .then(employee => res.send({success:true, employee}))
+    .catch(res.onError);
+});
+
+adminRouter.post('/signin',(req, res, next) => {
+    const {email,password} = req.body;
+    EmployeeService.SignIn(email,password)
+    .then(employee => res.send({success:true, employee}))
+    .catch(res.onError);
+});
 module.exports = {adminRouter};
