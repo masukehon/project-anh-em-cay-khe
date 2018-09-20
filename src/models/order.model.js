63723a12c555ebf1db17f4b7b2a1fc63f8ed27dd
Huu <nguyentienhuu47@gmail.com>
2018-09-20 16:05:35 +0700

    form 3

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -6,6 +6,7 @@ const orderSchema = new Schema({
     customerName: { type: String, trim: true, required: true },
     email: { type: String, trim: true, required: true },
     phone: { type: String, trim: true, required: true },
+    RequiredDate:{ type :date,trim: true, required: true },
     isSeen: { type: Boolean, required: true}
 });
 
