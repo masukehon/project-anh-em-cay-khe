1641ee4532b0e5db24e04842aef9e7efc2a0dada
Kha Cao Vinh <masuke96@gmail.com>
2018-09-26 17:12:16 +0700

    fix

--- src/controllers/admin/category.route.js
+++ src/controllers/admin/category.route.js
@@ -3,22 +3,22 @@ const { CategoryService } = require("../../services/category.service");
 const { Category } = require("../../models/category.model");
 const cateRouter = Router();
 
-// cateRouter.get('/', (req, res) => {
-//     CategoryService.getAll()
-//     .then(cate => res.send({success: true, cate}))
-//     .catch(res.onError);
-// });
+cateRouter.get('/', (req, res) => {
+    CategoryService.getAll()
+    .then(cate => res.send({success: true, cate}))
+    .catch(res.onError);
+});
 
-// cateRouter.get('/create', (req, res) => {
-//     CategoryService.createAll()
-//     .then(cate => res.send({success: true, cate}))
-//     .catch(res.onError);
-// });
+cateRouter.get('/create', (req, res) => {
+    CategoryService.createAll()
+    .then(cate => res.send({success: true, cate}))
+    .catch(res.onError);
+});
 
-// cateRouter.get('/remove', (req, res) => {
-//     CategoryService.removeAll()
-//     .then(cate => res.send({success: true, cate}))
-//     .catch(res.onError);
-// });
+cateRouter.get('/remove', (req, res) => {
+    CategoryService.removeAll()
+    .then(cate => res.send({success: true, cate}))
+    .catch(res.onError);
+});
 
 module.exports = { cateRouter };
