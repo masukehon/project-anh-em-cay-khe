5c3cfa8b3c80b180d33cd087d156a7759c1fc4d2
Huu <nguyentienhuu47@gmail.com>
2018-09-28 11:50:35 +0700

    deloy 2

--- src/controllers/admin/order.route.js
+++ src/controllers/admin/order.route.js
@@ -23,7 +23,9 @@ orderRouter.get('/:id', (req, res, next) => {
 orderRouter.post('/create', (req, res, next) => {
     const {name, email, phone} = req.body;
     OrderService.Create(name, email, phone, req)
-    .then(orders => res.status(200).send({success:true, orders}))
+    .then(orders => {
+        console.log('thanh cong');
+    })
     .catch(res.onError);
 });
 
