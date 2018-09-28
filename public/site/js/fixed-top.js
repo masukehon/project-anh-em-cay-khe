12c9d328c42e339a3fd47462845991cf5ca5d217
Kha Cao Vinh <masuke96@gmail.com>
2018-09-28 10:28:34 +0700

    final 2

--- public/site/js/fixed-top.js
+++ public/site/js/fixed-top.js
@@ -5,7 +5,7 @@
 				$(".site-header-main-wrapper").addClass("fixed-top").css({"color":"black","transition":"1s","background":"black","opacity":"0.8"});
 				}
 			else {
-				$(".site-header-main-wrapper").removeClass("fixed-top").css({"opacity":"0.8","height":"60px"});
+				$(".site-header-main-wrapper").removeClass("fixed-top").css({"opacity":"0.8","height":"60px","margin-top":"-6px"});
 				}
 		});
 	});
