ba0d6271e4f0d1c9faa1d6d7aedb459befb45dd5
Kha Cao Vinh <masuke96@gmail.com>
2018-09-29 13:17:03 +0700

    dslfkj

--- src/helpers/connectDB.js
+++ src/helpers/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function getDatabaseUri(){
-    if(process.env.PORT) return "mongodb://anhemcaykhe:huutien01@ds113853.mlab.com:13853/purtier";
+    if(process.env.PORT) return "mongodb://kha:123456kha@ds113853.mlab.com:13853/purtier";
     return "mongodb://localhost/myproject";
 }
 mongoose.connect(getDatabaseUri(),{ useNewUrlParser: true })
