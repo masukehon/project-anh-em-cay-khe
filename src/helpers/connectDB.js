44be040a42635e04f62ade9571675e892d655ca8
Kha Cao Vinh <masuke96@gmail.com>
2018-10-01 09:51:38 +0700

    add tawk chat

--- src/helpers/connectDB.js
+++ src/helpers/connectDB.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-function getDatabaseUri(){
-    if(process.env.PORT) return "mongodb://kha:123456kha@ds113853.mlab.com:13853/purtier";
+function getDatabaseUri() {
+    if (process.env.PORT) "mongodb://kha:123456kha@ds115283.mlab.com:15283/project-aeck";
     return "mongodb://localhost/myproject";
 }
-mongoose.connect(getDatabaseUri(),{ useNewUrlParser: true })
-.then(() => console.log('Database connected!!'))
-.catch(error => {
-    console.log(error);
-    process.exit(1);
-});
+mongoose.connect(getDatabaseUri(), { useNewUrlParser: true })
+    .then(() => console.log('Database connected!!'))
+    .catch(error => {
+        console.log(error);
+        process.exit(1);
+    });
 
